640f7d2cfc405f07634de10f80d6d39514175cee
Giovanny Arturo Gutierrez Estupinan <Ggutierrez@RUNT.LOC>
2021-06-15 14:37:16 -0500

    empaquetado angular-spring
    despliegue war sobre wildfly

---
+++ empaquetado/src/main/java/com/giovanny/empaquetado/EmpaquetadoApplication.java
@@ -0,0 +1,13 @@
+package com.giovanny.empaquetado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpaquetadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpaquetadoApplication.class, args);
+	}
+
+}
