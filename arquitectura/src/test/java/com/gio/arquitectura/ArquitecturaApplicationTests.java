8e0f15384d929f0f94797285232fbe968ed4404f
Giovanny Arturo Gutierrez Estupinan <Ggutierrez@RUNT.LOC>
2021-06-21 17:20:17 -0500

    Plantilla arquitectura spring
    Plantilla arquitectura proyecto web alta disponibilidad y transaccionalidad spring boot

---
+++ arquitectura/src/test/java/com/gio/arquitectura/ArquitecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gio.arquitectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
